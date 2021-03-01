2106983968944bdc81e186725c5eef3fc195c8a8
Adan de Oliveira <anonymu616@gmail.com>
2021-03-01 14:17:35 -0300

    Average 1
    Signed-off-by: Adan de Oliveira <anonymu616@gmail.com>

---
+++ URI/4-average_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+    cout << fixed;
+    cout << setprecision(5);
+    cout << "MEDIA = " << ((a * 3.5) + (b * 7.5)) / (3.5 + 7.5) << endl;
+    return 0;
+}
+
+//  cout << fixed << endl;
+//  cout << setprecision(4) << endl;
+//  cout << d << endl;
